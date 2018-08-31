6c6fb6854a71caba99c0e8fb239485046f9b1a47
Pranay Kothapalli <pranaykothapalli@gmail.com>
2018-08-31 16:15:32 +0530

    fix: update vue template project to fit Scout

--- vue-scout/src/App.vue
+++ vue-scout/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" style="display:block;margin:auto;margin-bottom:40px;">
+    <h1 style="text-align:center;">Vue Scout</h1>
+    <div class="vue-scout-container">
+          <Scout msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scout from './components/Scout.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Scout
+  },
+  data(){
+    return{
+      selected:[],
+      options:[]
+    }
+    
+  },
+  mounted(){
+    var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://reqres.in/api/products/3", true);
+xhr.onload = function(){
+    var res = JSON.parse(xhr.responseText);
+    console.log(res.data)
+};
+xhr.send();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.vue-scout-container{
+  width:500px;
+  margin:auto;
 }
 </style>
